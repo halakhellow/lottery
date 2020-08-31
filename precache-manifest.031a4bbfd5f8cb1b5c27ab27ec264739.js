d455d22c92f6f7c73adb842364954e73b9e475d2
Hala Alkhellow <halakhellow1996@gmail.com>
2020-08-31 21:22:07 +0300

    Updates

---
+++ precache-manifest.031a4bbfd5f8cb1b5c27ab27ec264739.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2890595083695079af2d658ecba89d19",
+    "url": "/lottery/index.html"
+  },
+  {
+    "revision": "e576be10b17b50556fc3",
+    "url": "/lottery/static/css/main.83f9064d.chunk.css"
+  },
+  {
+    "revision": "a38a2373699fa29a277f",
+    "url": "/lottery/static/js/2.5542dcff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lottery/static/js/2.5542dcff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e576be10b17b50556fc3",
+    "url": "/lottery/static/js/main.287ce0cb.chunk.js"
+  },
+  {
+    "revision": "ef40961be731c1fa82f7",
+    "url": "/lottery/static/js/runtime-main.e880633e.js"
+  }
+]);
